c6dc6bcacf7a2e6e2e631b735732d35ec5e5f0bc
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-15 16:29:09 -0600

    Update main.py

--- functions/delete-note/main.py
+++ functions/delete-note/main.py
@@ -1,1 +1,19 @@
 # add your delete-note function here
+
+import json 
+
+def delete_handler(event, context):
+    http_method = event["requestContext"]["http"]["method"].lower()
+    invoker = None
+
+    if http_method == "delete":
+        # DELETE use query string parameters to get info about the request
+        invoker = event["queryStringParameters"]["invoker"]
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "method": http_method,
+            "invoker": invoker
+        })
+    }
