66f79027e46f1f1e15810c3e6e3b238de27fdc2f
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-15 16:29:29 -0600

    Update main.py

--- functions/get-notes/main.py
+++ functions/get-notes/main.py
@@ -1,1 +1,19 @@
 # add your get-notes function here
+
+import json
+
+def get_handler(event, context):
+    http_method = event["requestContext"]["http"]["method"].lower()
+    invoker = None
+
+    if http_method == "get":
+        # GET use query string parameters to get info about the request
+        invoker = event["queryStringParameters"]["invoker"]
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "method": http_method,
+            "invoker": invoker
+        })
+    }
