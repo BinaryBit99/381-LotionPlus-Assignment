36d6e9b6adb460c9b17f47668dddee4490beac93
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-15 16:32:13 -0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -15,8 +15,12 @@ provider "aws" {
 # the locals block is used to declare constants that 
 # you can use throughout your code
 locals {
-  function_name = "hello-world"
-  handler_name  = "main.handler"
+  delete_function_name = "delete"
+  save_function_name = "save"
+  get_function_name = "get"
+  delete_note_handler_name  = "main.delete_handler"
+  get_notes_handler_name = "main.get_handler"
+  save_note_handler_name = "main.save_handler"
   artifact_name = "${local.function_name}/artifact.zip"
 }
 
@@ -51,17 +55,43 @@ EOF
 
 # create a Lambda function
 # see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
-resource "aws_lambda_function" "lambda" {
+resource "aws_lambda_function" "get-notes-<UCID>" {
   s3_bucket = aws_s3_bucket.lambda.bucket
   # the artifact needs to be in the bucket first. Otherwise, this will fail.
   s3_key        = local.artifact_name
   role          = aws_iam_role.lambda.arn
-  function_name = local.function_name
-  handler       = local.handler_name
+  function_name = local.delete_function_name
+  handler       = local.get_notes_handler_name
 
   # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
   runtime = "python3.9"
 }
+
+resource "aws_lambda_function" "save-note-<UCID>" {
+  s3_bucket = aws_s3_bucket.lambda.bucket
+  # the artifact needs to be in the bucket first. Otherwise, this will fail.
+  s3_key        = local.artifact_name
+  role          = aws_iam_role.lambda.arn
+  function_name = local.save_function_name
+  handler       = local.save_note_handler_name
+
+  # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
+  runtime = "python3.9"
+}
+
+resource "aws_lambda_function" "delete-note-<UCID>" {
+  s3_bucket = aws_s3_bucket.lambda.bucket
+  # the artifact needs to be in the bucket first. Otherwise, this will fail.
+  s3_key        = local.artifact_name 
+  role          = aws_iam_role.lambda.arn
+  function_name = local.delete_function_name
+  handler       = local.delete_note_handler_name
+
+  # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
+  runtime = "python3.9"
+}
+
+
 
 # create a policy for publishing logs to CloudWatch
 # see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
@@ -95,8 +125,94 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 }
 
 # output the name of the bucket after creation
-output "bucket_name" {
-  value = aws_s3_bucket.lambda.bucket
+# output "bucket_name" {
+#   value = aws_s3_bucket.lambda.bucket
+# }
+
+# create a Function URL for Lambda 
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function_url
+resource "aws_lambda_function_url" "save-url" {
+  function_name      = aws_lambda_function.save-note-<UCID>.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["POST", "PUT"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"] #Can be empty array
+  }
 }
 
 
+resource "aws_lambda_function_url" "delete-url" {
+  function_name      = aws_lambda_function.delete-note-<UCID>.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["DELETE"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+resource "aws_lambda_function_url" "get-notes-url" {
+  function_name      = aws_lambda_function.get-notes-<UCID>.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+# show the Function URL after creation
+output "get_notes_url_output" {
+    value = aws_lambda_function_url.get-notes-url.function_url
+}
+
+output "delete_note_url_output" {
+    value = aws_lambda_function_url.delete-url.function_url
+}
+
+output "save_note_url_output" {
+    value = aws_lambda_function_url.save-url.function_url
+}
+
+
+# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+resource "aws_dynamodb_table" "table-<UCID>" {
+  name         = "students"
+  billing_mode = "PROVISIONED"
+
+  # up to 8KB read per second (eventually consistent)
+  read_capacity = 1
+
+  # up to 1KB per second
+  write_capacity = 1
+
+  # we only need a student id to find an item in the table; therefore, we 
+  # don't need a sort key here
+  hash_key = "email"
+  range_key = "id"
+
+  # the hash_key data type is string
+  attribute {
+    name = "title"
+    type = "S"
+  }
+
+  attribute {
+    name = "text"
+    type = "S"
+  }
+  attribute {
+    name = "date"
+    type = "S"
+  }
+}
