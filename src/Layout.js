5bd75d27d14187af59b56c7a808532a1eaba5d80
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-15 16:25:58 -0600

    Update Layout.js
    commit with google login module

--- src/Layout.js
+++ src/Layout.js
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import { useNavigate, useParams } from "react-router-dom";
 import NotesList from "./NotesList";
 import { v4 as uuidv4 } from "uuid";
+import { googleLogout, useGoogleLogin, GoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 function Layout() {
   const navigate = useNavigate();
@@ -37,11 +39,60 @@ function Layout() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(notes));
   }, [notes]);
 
+  const [ user, setUser ] = useState([]);
+  const [ profile, setProfile ] = useState([]);
+
+  const login = useGoogleLogin({
+      onSuccess: (codeResponse) => setUser(codeResponse),
+      onError: (error) => console.log('Login Failed:', error)
+  });
+
+  useEffect(
+    () => {
+        if (user) {
+            axios
+                .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                    headers: {
+                        Authorization: `Bearer ${user.access_token}`,
+                        Accept: 'application/json'
+                    }
+                })
+                .then((res) => {
+                    setProfile(res.data);
+                })
+                .catch((err) => console.log(err));
+        }
+    },
+    [ user ]
+  );
+
+
+// log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+      googleLogout();
+      setProfile(null);
+  };
+
   return (
+    
     <>
       <div id="title">
         <h1>Lotion</h1>
         <p>Like Notion, but worse (like way worse)</p>
+        <p>React Google Login</p>
+            
+            {profile ? (
+                <div className="userLogin">
+          
+                    <p> Welcome back, {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                   
+                    <button onClick={logOut}>Log out</button>
+                </div>
+            ) : (
+                <button onClick={() => login()}>Sign in with Google 🚀 </button>
+            )}
+      
       </div>
       <label id="menu" onClick={showSidebar}>
         &#9776;
