5316a36423fb20a9b6ab27af9c5fdefa36697db3
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-15 16:29:59 -0600

    Update main.py

--- functions/save-note/main.py
+++ functions/save-note/main.py
@@ -1,1 +1,21 @@
 # add your save-note function here
+
+# add your save-note function here
+import json
+
+def save_handler(event, context):
+    http_method = event["requestContext"]["http"]["method"].lower()
+    invoker = None
+
+    if http_method == "post" or http_method == "put":
+        # POST and PUT use the request body to get info about the request
+        body = json.loads(event["body"])
+        invoker = body["invoker"]
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "method": http_method,
+            "invoker": invoker
+        })
+    }
